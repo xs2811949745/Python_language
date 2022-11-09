151142a18817261d310e1ab0895e545c8a8449a0
xs2811949745 <banXD1314>
2022-11-09 23:21:12 +0800

    python初学 因为下段时间考试不能每天学习

---
+++ learning.py
@@ -0,0 +1,23 @@
+# # a=str(111)
+# # print(type(a),a)
+# #print(5/2)
+# print("数字无法与字符串拼接")
+# str1=input()
+# str2=input()
+# print("世界那么大，{}想去{}看看".format(str1,str2))
+# n=input()
+# sum=0
+# for i in range(int (n)):
+#     sum+=i+1
+#     print('1到{}求和结果为：'.format(n),sum)
+#关于python输出字符缩进和格式不同问题
+# for i in range(1,10):
+#     for j in range(i, 10):
+#         print('{}*{}={:2}'.format(j, i, i * j), end='   ')
+#         print()
+# for i in range(1,10):
+#     for j in range(i,10):
+#         print('{}*{}={:2}'.format(j, i, i * j),end='  ')
+#     print()
+# w="星期一星期二星期三星期四星期五星期六"
+# print("星期五")
