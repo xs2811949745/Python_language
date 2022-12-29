dde6b618988e00023681e9d30b3598f43a2f9b71
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2022-12-29 21:33:57 +0800

    Add files via upload

---
+++ learn_day3.py
@@ -0,0 +1,27 @@
+# name="hmcxy"
+# massage="学it来%s"
+# print(massage%name)
+# a=100
+# b=100.33
+# c="%d+%f"%(a,b)
+# print(c)
+# name="传递博客"
+# set_up_year=2006
+# stock_price=19.99
+# print(f"{name}成立于{set_up_year},股价是{stock_price}")
+# name = '传智播客'
+# stock_price = 19.99
+# stock_code = '003032'
+# stock_price_daily_growth_factor = 1.2
+# growth_day = 7
+# print(f"公司{name},股票代码{stock_code},当前股价{stock_price}")
+# print("每日增长系数是%.1f,经过%d天增长后，股价达到了%5.2f"%(stock_price_daily_growth_factor,growth_day,stock_price*1.2**7))
+a=int(input())
+b=int(input())
+c=a+b
+# c=int(a)+int(b)
+print(c)
+# # a=int(a) b=int(b)
+# print(c)
+# name = input("请输入你是谁？")
+# print(name)
