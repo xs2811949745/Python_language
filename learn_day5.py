230aa8f208f5a3ec40e9fa348e9ef4e418b6c3a0
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2023-01-29 23:04:04 +0800

    Add files via upload

---
+++ learn_day5.py
@@ -0,0 +1,16 @@
+# print("aaabbb\n"*100)
+# 520+1314
+# '520'+'1314'
+#
+
+# str1="Hello World!"
+# str2="Hello Nowcoder!"
+# print(str1,\n
+# str2)
+# str1="Hello World!"
+# str2="Hello Nowcoder!"
+# print(str1,'\n',str2)
+# a=input()
+# print({a}+"\n"+{type(a)})
+# name=input()
+# print("I am"+name+" and I am studying Python in Nowcoder!")
