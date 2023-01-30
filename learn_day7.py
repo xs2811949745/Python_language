2b8a5b1c5ba1f56dd730529f2d89584bd6f1bb73
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2023-01-30 20:34:53 +0800

    Add files via upload

---
+++ learn_day7.py
@@ -0,0 +1,18 @@
+# a=1+10
+# if a>3:
+#     print(a)
+#     print(666)
+# else:
+#     print(0)
+# print("请欢迎来到黑马儿童游乐场,儿童免费,成人收费. ")
+# age=int(input("请输入你的年龄:"))
+# # age=int(age)
+# if age >= 18:
+#     print("您已成年,请补票十元")
+# print("祝您游玩愉快")
+#
+age=int(input("欢迎来到黑马游乐园"+'\n'+"请输入您的身高"))
+if age>=120:
+    print("10")
+else:
+    print(0)
