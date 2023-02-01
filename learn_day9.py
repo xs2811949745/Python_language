3fa20744c2bf8a93eb53117ffa442eef26f4e269
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2023-02-01 20:30:44 +0800

    Add files via upload

---
+++ learn_day9.py
@@ -0,0 +1,51 @@
+# coding=gbk
+# name="itheima is a brand of itcast"
+# count=0
+# for a in name:
+#     if a=='a':
+#        count+=1
+# print(count)
+# for x in range(5,10,2):
+#     print (x)
+# for x in range(5,10):
+#     print(x)
+# for x in range (10):
+#     print(x)
+# import random
+# count=0
+# num=random.randint(1,1000)
+# for x in range(1,num):
+#     if x%2==0:
+#         count+=1
+# print(f"1-{num-1}偶数{count}个")
+# for x in range(1,10):
+#     for y in range(1,x+1):
+#         print(f"{x}*{y}={x*y}\t",end='')
+#     print()
+# balance=10000
+# for x in range(1,21):
+#     import random
+#     score=random.randint(1,11)
+#     if balance<=0:
+#         print("没钱了")
+#         break
+#     else:
+#         if score>=5:
+#             print(f"员工{x}发工资1000元,账户余额还剩{balance-1000}")
+#             balance-=1000
+#             continue
+#         else:
+#             print("绩效低于5不发工资")
+#
+
+
+
+
+
+
+
+
+
+
+
+
