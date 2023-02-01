ec499d01db9b75dcc1d6dbb0cebc890d4a24aba3
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2023-02-01 14:10:04 +0800

    Add files via upload

---
+++ learn_day8.py
@@ -0,0 +1,110 @@
+# print("welcome")
+# height=int(input("请输入身高"))
+# vip_lever=int(input("请输入vip等级"))
+# if height < 130:
+#     print("free")
+# elif vip_lever>3:
+#     print("free")
+# else:
+#     print(10)
+# print("happy")
+# age=int(input("输入年龄"))
+# if age>=18 and age<30:
+#     print("年龄达标")
+#     years=int(input("工作时长"))
+#     if years >= 2:
+#         print(6)19
+#
+#     else:
+#         lever=int(input("工作级别"))
+#         if lever>=3:
+#             print(6)
+#
+# import random
+# num=random.randint(1,10)
+# guess=int(input("第一次机会"+'\n'))
+# if guess == num:
+#     print(666)
+# else:
+#     # print(2"第二次机会"+'\n')
+#     guess = int(input("第二次机会" + '\n'))
+#     if guess == num:
+#         print(666)
+#     else:
+#         # print("第二次机会"+'\n')
+#         guess = int(input("第三次机会" + '\n'))
+#         if guess == num:
+#             print(666)
+
+# 改良后
+#
+# flag = True
+# count=0
+# import random
+# num=random.randint(1,100)
+#
+# while flag:
+#     count+=1
+#     guess=int(input("输入数字"))
+#     if guess==num:
+#         print("对了")
+#         print(f"猜了{count}次")
+#         flag=False
+#     elif guess>num:
+#         print("大了")
+#     elif guess<num:
+#         print("小了")
+# sum
+# sum=0
+# sum+=i
+# print(f"{type(i)},{type(sum)}")
+# i=1
+# sum=0
+# while i<=100:
+#     sum+=i
+#     i+=1
+# print(sum)
+# a=100
+# print(a%"成立于",a)
+# print("成立于%s"%a)
+# print("111"*10)
+# print("hello")
+# print("world")
+# print("hello",end='')
+# print( " world"+'\n',end='')
+# print("hello\tworld")
+#
+# print("hxm\tnbest")
+# i=1
+# while i<=9:
+#     j=1
+#     while j<=i:
+#         print(f"{j}*{i}={i*j}\t",end='')
+#         j+=1
+#     print()
+#     i+=1
+# name="qwertyu"
+# for a in name:
+#     print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
