8eac55ac296c0e1e2e23c1a5b90e49a5ec156caa
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2023-02-07 21:07:05 +0800

    Add files via upload

---
+++ learn_day11.py
@@ -0,0 +1,22 @@
+# coding=gbk
+name=["a",'b','c']
+print(name)
+b=[1,2,name]
+print(b)
+#²é
+a=name.index("a")
+print(a)
+#Ôö
+name.insert(1,2)
+print(name)
+name.append("Xye")
+
+print(name)
+# É¾
+del name[2]
+print(name)
+name.pop(2)
+print(name)
+# 
+
+
