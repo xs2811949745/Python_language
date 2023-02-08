01895d24398d8fce307b8c4b82e53ae89ac7045a
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2023-02-08 20:07:17 +0800

    Add files via upload

---
+++ test_1.py
@@ -0,0 +1,33 @@
+# coding=gbk
+# import sys
+#
+# for line in sys.stdin:
+#     a = line.split()
+#     print(int(a[0]) + int(a[1]))
+list=[21,25,21,23,22,20]
+# # 2追加31
+# list.append(31)
+# # 3追加列表
+# list.extend([29,33,30])
+# # 4取出第一个元素(21)
+# b=list.pop(0)
+# # del list[0]
+# print(b)
+# # 取出最后一个元素(30
+# c=list.pop(-1)
+# print(c)
+# # 查找31,返回下标
+# d=list.index(31)
+# print(d)
+
+# for x in range(0,len(list)):
+#     print(list[x])
+# for x in list:
+#     print(x)
+# list.clear()
+# list1=[0,1,2,3,4,5,6,7,8,9,10]
+# for x in list1:
+#     if x%2==0:
+#         print(x)
+#         list.append(x)
+# print(list)
