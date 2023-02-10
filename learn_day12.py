b1655c65c9c0a0df71a324ec45a18d0c9e1db180
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2023-02-10 20:23:35 +0800

    Add files via upload

---
+++ learn_day12.py
@@ -0,0 +1,61 @@
+# coding=gbk
+# a=tuple(1,2,3)
+# print(a)
+# a=("a")
+# print(type(a))
+# b=("a",)
+# print(type(b))
+# a=((1,2,3),(4,5,6))
+# print(a[0][2])
+# print(len(a))
+# count=a.index((4,5,6))
+# print(count)
+# num=a.count((1,2,3))
+# print(num)
+b=(1,2,3,4,5,6,7,8,9)
+# c=b.index(9)
+# print(c)
+# num=b.count(1)
+# print(num)
+# print(len(b))
+x=0
+# while x<len(b):
+#     print(b[x])
+#     x+=1
+# for x in b:
+#     print(x)
+# a=([1,2,3,4,5,6],4,5,6,7,8,9)
+# a[0].append(b)
+# a[0].extend([7,8,9,10])
+# print(a)
+# tuple=('周杰伦',11,["football",'music'])
+# print(tuple.index(11))
+# print(tuple[0])
+# tuple[2].pop(0)
+# print(tuple)
+# # tuple[2].append("coding")
+# tuple[2].insert(0,"coding")
+# print(tuple)
+a="12hxm is a xhx shsh sja xxxx111"
+# b=a.replace('x','6')
+# print(b)
+# list=a.split( )
+# print(list)
+# str=a.strip("12x")
+# print(a)
+# print(str)
+# a="itheima itcast boxiegu"
+# b=a.count("it")
+# print(b)
+# b=a.replace(' ','|')
+# print(b)
+# list=b.split("|")
+# print(list)
+# a=[0,1,2,3,4,5,6]
+# b=a[1:4]
+# print(b)
+# a="万过薪月,员序程马黑来,nohtyp学"
+# b=a[::-1]
+# print(b)
+# c=b[9:14:1]
+# print(c)
